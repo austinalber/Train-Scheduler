0b88c5508b9fba9d923adbeba8d4456ae743bdb2
Austin Alber <austinalber@gmail.com>
2019-07-23 15:27:41 -0400

    Removed calculations function and moved it to initial firebase loader function. Also added styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,52 +19,6 @@ var destination = "";
 var firstTrainTime = "";
 var frequency = 0;
 
-// Calculation Function to determine minutes left
-function calculations(firstTrainTime, frequency){
-
-    // Get Both Current Time and First Train Time in HH:mm
-    var now = moment(new Date()).format("HH:mm"); //todays date
-    var end = moment(firstTrainTime, "HH:mm").format("HH:mm"); // another date
-
-    // Split the two times and turn them into arrays with 2 point
-    var nowArray = now.split(":");
-    var endArray = end.split(":"); 
-    console.log("nowArray: " + nowArray); // 19:01 returns ["18", "53"]
-    console.log("endArray: " + endArray); // 12:15
-
-    // Find Difference between times
-
-    // If the minutes of the first arrival is greater than the minutes of the
-    // current time: subtract the difference and account for 60min per hour
-    endArray[1]= endArray[1]-nowArray[1];
-    endArray[0]= endArray[0]-nowArray[0];
-    console.log("New endArray: " + endArray);
-
-    if(endArray[0] < 0) {
-        endArray[0] = Math.abs(endArray[0]);
-    }
-    if(endArray[1] < 0) {
-        endArray[1] = Math.abs(endArray[1]);
-    }
-
-    console.log("endArray after Math.abs: " + endArray);
-
-    // While there is greater than 1 hour of time between the first arrival,
-    // remove a hour from the time and add in minutes
-    while(endArray[0] > 0)  {
-        endArray[0] = endArray[0] - 1;
-        endArray[1] = endArray[1] + 60;
-    }
-
-    console.log("endArray after while statement: " + endArray);
-
-    // Set the amount of minutes to a variable
-    var minutesRemaining = endArray[1];
-    console.log("Minutes Remaining until next Train: " + minutesRemaining);
-        
-    return minutesRemaining;
-}
-
 $("#submit").on("click", function(event)  {
         event.preventDefault();
 
@@ -94,24 +48,31 @@ dataRef.ref().on("child_added", function(childSnapshot) {
 
     // Save snapshot object as a variable
     var sv = childSnapshot.val();
-
-    // Grab info from Calculations
-    minutesRemaining = calculations(sv.firstTrainTime, sv.frequency, sv.dateAdded);
+    
+    // Calculations
+    var timeConversion = moment(childSnapshot.val().firstTrainTime, "hh:mm").subtract(1, "years");
+    var timeDifference = moment().diff(moment(timeConversion), "minutes");
+    var minutesRemaining = timeDifference % childSnapshot.val().frequency;
+    var minutesRemaining = childSnapshot.val().frequency - minutesRemaining;
+    var nextArrival = moment().add(minutesRemaining, "minutes");
+    nextArrival = moment(nextArrival).format("LT");
 
     // // Log everything that's coming out of snapshot
-    // console.log("Name: " + sv.name);
-    // console.log("Destination: " + sv.destination);
-    // console.log("First Train Time: " + sv.firstTrainTime);
-    // console.log("Frequency: " + sv.frequency);
+    console.log("Name: " + sv.name);
+    console.log("Destination: " + sv.destination);
+    console.log("First Train Time: " + sv.firstTrainTime);
+    console.log("Frequency: " + sv.frequency);
+    console.log("Next Arrival: " + nextArrival);
+    console.log("Minutes Remaining: " + minutesRemaining);
 
     // Template Variable
     var newEntry = `
     <tr>
-        <td>${sv.name}</td>
-        <td>${sv.destination}</td>
-        <td>${sv.frequency}</td>
-        <td>${sv.nextArrival}</td>
-        <td>${minutesRemaining}</td>
+        <td class="text-center">${sv.name}</td>
+        <td class="text-center">${sv.destination}</td>
+        <td class="text-center">${sv.frequency}</td>
+        <td class="text-center">${nextArrival}</td>
+        <td class="text-center">${minutesRemaining}</td>
     </tr>
     `
 
